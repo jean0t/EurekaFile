65ca8cebb6f1712bd98d6398105d5e05271c25ab
JMFern01 <joao.fernandes@gravata.pe.gov.br>
2025-07-08 11:45:23 -0300

    feat(index.go): index controller created

--- internal/controllers/index.go
+++ internal/controllers/index.go
@@ -2,6 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"html/template"
+	"path/filepath"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {}
+func Index(w http.ResponseWriter, r *http.Request) {
+	var templ = template.Must(template.ParseFiles(
+		filepath.Join("views", "login.tmpl"),
+	))
+
+	templ.ExecuteTemplate(w, "Login", nil)
+}
