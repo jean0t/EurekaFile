e31fd1d32b87eb16fcbf718c1448d9a0ab993822
JMFern01 <joao.fernandes@gravata.pe.gov.br>
2025-07-11 12:59:10 -0300

    feat: implementing creation of a session with jwt in login

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -2,8 +2,23 @@ package controllers
 
 import (
 	"net/http"
+	"github.com/jean0t/EurekaFile/internal/auth"
 )
 
-func Login(w http.ResponseWriter, r *http.Request) {}
+func Login(w http.ResponseWriter, r *http.Request) {
+	var cookie *http.Cookie
+	var err error
 
-func Logout(w http.ResponseWriter, r *http.Request) {}
+	cookie, err = r.Cookie("auth")
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	
+	
+	http.Redirect(w, r, "/upload", http.StatusSeeOther)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
